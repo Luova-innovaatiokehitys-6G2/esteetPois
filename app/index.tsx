b11bca53938da13db9070c77c466e1aeaa56a83a
IsakMakimartti <t2mais00@students.oamk.fi>
2026-03-29 15:58:34 +0300

    remove unused code

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,6 @@ import useUserCurrentLocation from "./hooks/userCurrentLocation";
 import Loading from "./loading";
 import BaseMap from "./baseMap"
 import NavigationMap from "./navigationMap";
-import fixedCoordinateList from "./hooks/fetchCoordinates";
 
 const Map = () => {
 
@@ -15,12 +14,7 @@ const Map = () => {
   const userLongitude: number = userLocation?.coords.longitude ?? 0;
   const userLocationFetched: boolean = userLongitude !== 0 && userLatitude !== 0;
 
-  const fixedCoordinates = fixedCoordinateList();
-  const oamkData = fixedCoordinates[0] ?? null;
-  const ideaparkData = fixedCoordinates[1] ?? null;
-  const destinationLocationsFetched: boolean = oamkData !== null && ideaparkData !== null;
-
-  if (!userLocationFetched && destinationLocationsFetched) return <Loading />;
+  if (!userLocationFetched) return <Loading />;
   if (navigationMapView && userLocationFetched) return <NavigationMap />;
 
   return <BaseMap navigationMapView={navigationMapView} onToggle={() => setNavigationMapView(prev => !prev)} />;
